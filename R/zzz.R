8eb9024d0b0d9dac891b4d4d59bfde75dcae02b4
Ousmane Ouedraogo <oousmane@proton.me>
2026-03-14 18:09:21 +0000

    enhancing startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,13 +2,13 @@
 #' @keywords internal
 .onAttach <- function(libname, pkgname) {
   version <- utils::packageVersion(pkgname)
-
   msg <- cli::format_inline(
     "{.pkg aquacropr} v{version}\n",
-    "Wrapper and Tools to streamline the use of FAO Crop Water Model AquaCrop (v7.0+)\n",
+    "R interface to AquaCrop v7 and higher, the FAO crop water productivity model.\n",
+    "Streamlines reading, writing, and managing AquaCrop input/output files\n",
+    "for batch simulations, climate scenario analysis, and crop yield assessment.\n",
     "\n",
-    "First time setup: use {.code init_aquacrop()} to create aquacropr project structure."
+    "First time setup: use {.code init_aquacrop()} to initialize your project."
   )
-
   packageStartupMessage(msg)
 }
